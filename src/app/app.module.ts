e0de4ed5757d542e4a75a7ff0059245aaf6650fc
cantwell136 <carrammrodd@live.com>
2020-07-20 23:24:22 -0500

    added Agm for maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AgmCoreModule } from '@agm/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './componets/user-profile/user-profile.component';
@@ -22,7 +24,10 @@ import { LoginComponent } from './componets/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyBy9IKBwV9Tt51GQzkZTAg98i7ecAxaAs0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componets/home/home.component.ts
+++ src/app/componets/home/home.component.ts
@@ -5,42 +5,10 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angula
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent
-  implements AfterViewInit {
+export class HomeComponent {
   title = 'globalMissions';
-  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
-  map: google.maps.Map;
   lat = -1.3667180;
   lng = 37.6021070;
 
-  coordinates = new google.maps.LatLng(this.lat, this.lng);
-
-  mapOptions: google.maps.MapOptions = {
-    center: this.coordinates,
-    zoom: 8
-  };
-
-  marker = new google.maps.Marker({
-    position: this.coordinates,
-    map: this.map,
-  });
-
-  ngAfterViewInit() {
-    this.mapInitializer();
-  }
-
-  mapInitializer() {
-    this.map = new google.maps.Map(this.gmap.nativeElement,
-      this.mapOptions);
-    this.marker.setMap(this.map);
-  }
+  
 }
- /* {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
- */
