aaf7f7c06b46c052e3e2c96348273dd7771638ee
scottyg68 <59403591+scottyg68@users.noreply.github.com>
2020-07-21 14:17:32 -0600

    Update sign-up.component.ts

--- src/app/componets/sign-up/sign-up.component.ts
+++ src/app/componets/sign-up/sign-up.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sign-up',
+  selector: 'app-signup',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
 title = 'Registration Page';
-  loginPath: string = 'http://localhost:3001/signup';
+  signupPath: string = 'http://localhost:3001/users/signup';
   constructor() { }
 
   ngOnInit(): void {
