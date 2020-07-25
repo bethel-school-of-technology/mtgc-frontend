71422df10a08ee7f2f581109d3851bbb0e2bf063
Connor Chaffin <cchaffin43@gmail.com>
2020-07-25 11:24:19 -0500

    update sign-in id's and names

--- src/app/componets/sign-up/sign-up.component.ts
+++ src/app/componets/sign-up/sign-up.component.ts
@@ -7,11 +7,14 @@ import { Users } from '../../models/users';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  user: Users = new Users();
 title = 'Registration Page';
   signupPath: string = 'http://localhost:3000/users/signup';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+ onSubmit(){
+   console.log('Submit successful: ', this.user);
+ }
 }
