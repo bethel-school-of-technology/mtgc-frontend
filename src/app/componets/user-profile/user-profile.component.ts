aac312b3d14002c7f6c511bca485591aa6328d78
cantwell136 <carrammrodd@live.com>
2020-07-31 19:28:48 -0500

    scotts missing link

--- src/app/componets/user-profile/user-profile.component.ts
+++ src/app/componets/user-profile/user-profile.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Users } from '../../models/users'
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  private userRoute = 'http://localhost:3000/users/profile';
+  public users: Users[];
+  constructor(private http: HttpClient) { }
+  getUsers() {
+    this.http.get<Users[]>(this.userRoute).subscribe(users => {
+        this.users = users;
+        console.log('Users', this.users);
+      });
+    }    
+  ngOnInit() {
+    this.getUsers();
   }
-
 }
