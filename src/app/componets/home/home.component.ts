67972467730a13e9b2e110acae01fdb342f42ba8
cantwell136 <carrammrodd@live.com>
2020-08-05 20:49:49 -0500

    pull for intergration

--- src/app/componets/home/home.component.ts
+++ src/app/componets/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MapsService } from '../../../services/maps.service'
+import { MapsService } from '../../maps.service';
 // import { MapCardComponent } from '../map-card/map-card.component';
 
 @Component({
@@ -13,16 +13,30 @@ export class HomeComponent {
   lng: string ='';
  
   location: object;
+
+  mklat: string ='';
+  mklng: string ='';
+
+  markerLocation: object;
   
   constructor(private map: MapsService) {}
 
   ngOnInit() {
-    this.map.userLocation().subscribe(data =>{
+    /* this.map.userLocation().subscribe(data =>{
     console.log(data);
     this.lat = data.latitude;
     this.lng = data.longitude;  
+    }) */
+
+    
+
+    this.map.missionsLocation().subscribe(location =>{
+      console.log(location);
+      this.mklat = location.latitude;
+      this.mklng = location.longitude;
     })
     
   }
   
+
 }
