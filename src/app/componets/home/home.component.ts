b81450eabb44b393b27233ff6f377faf64e85c8b
cantwell136 <carrammrodd@live.com>
2020-08-09 16:20:36 -0500

    Map Markers

--- src/app/componets/home/home.component.ts
+++ src/app/componets/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MapsService } from '../../maps.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 // import { MapCardComponent } from '../map-card/map-card.component';
 
 @Component({
@@ -18,22 +19,23 @@ export class HomeComponent {
   mklng: string ='';
 
   markerLocation: object;
-  
+
   constructor(private map: MapsService) {}
 
   ngOnInit() {
-    /* this.map.userLocation().subscribe(data =>{
+    this.map.userLocation().subscribe(data =>{
     console.log(data);
     this.lat = data.latitude;
     this.lng = data.longitude;  
-    }) */
+    })
 
     
 
-    this.map.missionsLocation().subscribe(location =>{
-      console.log(location);
-      this.mklat = location.latitude;
-      this.mklng = location.longitude;
+    this.map.missionsLocation().subscribe((location: any) =>{
+      console.log(location.results);
+     /*  this.mklat = location.latitude;
+      this.mklng = .longitude; */
+      this.markerLocation = location.results
     })
     
   }
